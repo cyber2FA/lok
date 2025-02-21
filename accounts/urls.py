43b5df1d4332046f2400c107e032c577c791f319
cyber2FA <SecureAuthSys@gmail.com>
2025-02-21 17:34:11 +0300

    16

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import register, homepage, dashboard  # استيراد جميع الفيوهات المطلوبة
-from django.contrib.auth.views import LoginView  # استيراد فيو تسجيل الدخول  الجاهز من Django
+from django.contrib.auth.views import LoginView  # استيراد فيو تسجيل الدخول   الجاهز من Django
 
 urlpatterns = [
     path('', homepage, name='homepage'),  # الصفحة الرئيسية
